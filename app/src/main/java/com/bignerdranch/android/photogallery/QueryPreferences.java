2ecf3cc9322c7e859395e6363d6d9f0b73b8587b
Edman Feng <edmanfeng@hotmail.com>
2017-05-29 04:00:14 -0700

    27 done

---
+++ app/src/main/java/com/bignerdranch/android/photogallery/QueryPreferences.java
@@ -0,0 +1,40 @@
+package com.bignerdranch.android.photogallery;
+
+import android.content.Context;
+import android.preference.PreferenceManager;
+
+/**
+ * Interface for reading and writing the query to and from shared preferences
+ * Allows for persistence of your query
+ */
+
+
+
+public class QueryPreferences {
+
+    // Key for query preference
+    private static final String PREF_SEARCH_QUERY = "searchQuery";
+
+    /**
+     * Returns the query value stored in shared preferences for the
+     * given context.
+     * @param context The context to get the SharedPreferences from.
+     * @return The query value.
+     */
+    public static String getStoredQuery(Context context) {
+        return PreferenceManager.getDefaultSharedPreferences(context)
+                .getString(PREF_SEARCH_QUERY, null);
+    }
+
+    /**
+     * Writes the query to the shared preferences for the given context.
+     * @param context The context to get the shared preferences from
+     * @param query The query to write
+     */
+    public static void setStoredQuery(Context context, String query) {
+        PreferenceManager.getDefaultSharedPreferences(context)
+                .edit()
+                .putString(PREF_SEARCH_QUERY, query)
+                .apply();
+    }
+}
